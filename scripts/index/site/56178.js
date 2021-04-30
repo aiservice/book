ff42271ebc34d36b082cf3c7bf04daa6dea23995
yangwu3 <yangwu3@cisco.com>
2021-04-30 09:33:53 +0800

    recover to biquge

--- scripts/index/site/56178.js
+++ scripts/index/site/56178.js
@@ -1,10 +1,10 @@
-var app_domain = "www.sobqg.com";var is_english = false;var app_link_email = "1740933979@qq.com";var site_id = 56178;var baid = "6ff55ceec79bc983ff3933a2a98e872f";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
+var app_domain = "www.biquge.work";var is_english = false;var app_link_email = "1740933979@qq.com";var site_id = 56178;var baid = "6ff55ceec79bc983ff3933a2a98e872f";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
 g_data_ad_slot_468 = "2008793013";
 g_data_ad_slot_recommend = "7657556062";
 g_data_ad_client = "ca-pub-4603891518763240";
 
 filterUrls = ["cate-5008","category-5008"];
 
-siteGUrls = ["aishen","feiku","shitou"];
-siteOtherUrls = ["aishen","feiku","shitou"];
+siteGUrls = ["aishen","feiku","shitou","biquge"];
+siteOtherUrls = ["aishen","feiku","shitou","biquge"];
 
