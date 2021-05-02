87b63ee64d389e4c05b10edca7374b8dcf8bb65b
yangwu3 <yangwu3@cisco.com>
2021-05-02 18:44:09 +0800

    fix baidu tss autio bug

--- scripts/index/book.audio.source.js
+++ scripts/index/book.audio.source.js
@@ -16,24 +16,24 @@ AudioUtils = {
         $mark: null,
         $results: null,
         currentClass: "current",
-        offsetTop :50,
-        currentIndex : 0
+        offsetTop: 50,
+        currentIndex: 0
     },
     playSpeechElement: null,
     $content: null,
-    url        : "//tts.baidu.com/text2audio?cuid=baiduid&lan=zh&ctp=1&pdt=311&spd={spd}&per={per}&tex=",
-    urlList    :[],
-    chunkList  :[],
-    audioList  :[],
-    curIndex  : 0,
-    isFirstPlay  : true,
-    isMobile  : false,
+    url: "//tts.baidu.com/text2audio?cuid=baiduid&lan=zh&ctp=1&pdt=31&spd={spd}&per={per}&tex=",
+    urlList: [],
+    chunkList: [],
+    audioList: [],
+    curIndex: 0,
+    isFirstPlay: true,
+    isMobile: false,
     init: function () {
-        this.urlList    = [],
-            this.chunkList  =[], this.audioList  =[], this.curIndex = 0, this.isFirstPlay  = true;
+        this.urlList = [],
+            this.chunkList = [], this.audioList = [], this.curIndex = 0, this.isFirstPlay = true;
         this.playSpeechElement.data("status", 0);
     },
-    jumpToMark :function() {
+    jumpToMark: function () {
         var that = this;
         //var lang = i18nUtils.getDefaultLocale();
         if (that.mark.$results.length) {
@@ -46,12 +46,12 @@ AudioUtils = {
             }
         }
     },
-    markText:function(searchVal){
+    markText: function (searchVal) {
         var that = this;
         that.mark.$mark.unmark();
         that.mark.$mark.mark(searchVal, {
             separateWordSearch: false,
-            done: function() {
+            done: function () {
                 that.mark.$results = that.$content.find("mark");
                 //that.mark.$results.addClass(that.mark.currentClass);
                 that.mark.currentIndex = 0;
@@ -59,89 +59,89 @@ AudioUtils = {
             }
         });
     },
-    play:function(){
+    play: function () {
         var that = this;
         var searchText = that.chunkList[that.curIndex];
-        console.log("==play==" ,that.curIndex , searchText);
+        console.log("==play==", that.curIndex, searchText);
         try {
             var promise = that.audioList[0].play();
             that.markText(searchText);
             this.playSpeechElement.data("status", 1);
             this.playSpeechElement.next("label").text(i18nUtils.prop("book_audio_pause"));
             if (promise !== undefined) {
-                promise.catch(function(e) {
+                promise.catch(function (e) {
                     that.alert(i18nUtils.prop("book_audio_auto_play_error"));
                     that.pause();
                 })
             }
-        } catch(err) {
+        } catch (err) {
             this.alert("Play error:" + err.message)
         }
 
     },
-    pause:function(){
+    pause: function () {
         console.log("==pause==");
         var that = this;
         try {
-            $.storage("auto-play","false");
-        } catch(err) {
-            var txt ="set not auto play failed：" + err.message+"\n\n";
-            that.alert(txt,2);
+            $.storage("auto-play", "false");
+        } catch (err) {
+            var txt = "set not auto play failed：" + err.message + "\n\n";
+            that.alert(txt, 2);
         }
         try {
             this.audioList[0].pause();
             this.playSpeechElement.data("status", -1);
             this.playSpeechElement.next("label").text(i18nUtils.prop("book_audio_play"));
-        } catch(err) {
+        } catch (err) {
             this.alert("Pause failed:" + err.message)
         }
     },
-    seek:function(i){
+    seek: function (i) {
         this.curIndex = Math.min(i, Math.max(this.urlList.length - 1, 0))
         //console.log("==seek:{}, curIndex:{}", i, this.curIndex);
     },
-    load:function(i){
+    load: function (i) {
         var index = Math.min(this.curIndex + i, this.urlList.length - 1);
-        var urlTemp =  this.urlList[index];
-        if((i == 0 && this.isFirstPlay) || i == 1){
-            console.log("==load index:{}, per:{}, spd:{}", index, this.params.per,this.params.spd);
-            urlTemp =  urlTemp.replace("{per}",this.params.per);
-            urlTemp =  urlTemp.replace("{spd}",this.params.spd);
+        var urlTemp = this.urlList[index];
+        if ((i == 0 && this.isFirstPlay) || i == 1) {
+            console.log("==load index:{}, per:{}, spd:{}", index, this.params.per, this.params.spd);
+            urlTemp = urlTemp.replace("{per}", this.params.per);
+            urlTemp = urlTemp.replace("{spd}", this.params.spd);
             console.log("==load index:{}, url:{} isFirstPlay:", index, urlTemp, this.isFirstPlay);
             this.audioList[i].src = urlTemp
         }
     },
-    loadAll:function(){
+    loadAll: function () {
         var i = this;
         this.audioList.forEach(function (t, e) {
             console.log("==loadAll e:{}", e);
             i.curIndex + e < i.urlList.length && i.load(e)
         })
     },
-    onAudioEnded:function(){
+    onAudioEnded: function () {
         //console.log("==onAudioEnded="+ this.curIndex);
         var that = this;
-        if(this.curIndex + 1 < this.urlList.length){
+        if (this.curIndex + 1 < this.urlList.length) {
             this.audioList.push(this.audioList.shift());
             this.curIndex += 1;
             this.playAudio(this.curIndex)
-        }else{
+        } else {
             console.log("==all audio played==")
             that.mark.$mark.unmark();
             try {
                 var isAutoPlay = $.storage("auto-play");
-                if(isAutoPlay === "true"){
+                if (isAutoPlay === "true") {
                     that.goNextChapter();
                 } else {
                     if (that.isMobile) {
                         layer.open({
                             content: i18nUtils.prop("book_audio_play_end")
-                            ,btn: [i18nUtils.prop("book_audio_play_auto"), i18nUtils.prop("book_audio_play_manual")]
-                            ,yes: function(index){
+                            , btn: [i18nUtils.prop("book_audio_play_auto"), i18nUtils.prop("book_audio_play_manual")]
+                            , yes: function (index) {
                                 $.storage("auto-play", "true");
                                 that.goNextChapter();
                                 layer.close(index);
-                            },no:function(index){
+                            }, no: function (index) {
                                 that.goNextChapter();
                                 layer.close(index);
                             }
@@ -149,17 +149,17 @@ AudioUtils = {
                     } else {
                         layer.confirm(i18nUtils.prop("book_audio_play_end"), {
                             btn: [i18nUtils.prop("book_audio_play_auto"), i18nUtils.prop("book_audio_play_manual")]
-                        }, function(){
+                        }, function () {
                             $.storage("auto-play", "true");
                             that.goNextChapter();
-                        }, function(){
+                        }, function () {
                             that.goNextChapter();
                         });
                     }
                 }
-            } catch(err) {
-                var txt ="not support cookie：" + err.message+"\n\n";
-                that.alert(txt,2);
+            } catch (err) {
+                var txt = "not support cookie：" + err.message + "\n\n";
+                that.alert(txt, 2);
             }
         }
 
@@ -168,25 +168,25 @@ AudioUtils = {
         //     this.audioList.length - 1), this.playAudio(this.curIndex)) : (this.pause(), this.seek(0),
         //     this.loadAll())
     },
-    goNextChapter:function(){
+    goNextChapter: function () {
         var that = this;
         var nextUrl = $("#nexturl").attr("href");
         if (nextUrl !== "javascript:void(0);") {
-            if(nextUrl.indexOf(";") !== -1){
-                nextUrl = nextUrl.substring(0,nextUrl.indexOf(";") );
+            if (nextUrl.indexOf(";") !== -1) {
+                nextUrl = nextUrl.substring(0, nextUrl.indexOf(";"));
             }
             window.location.href = nextUrl;
         } else {
             that.pause();
-            this.alert(i18nUtils.prop("book_is_last_chapter"),2);
+            this.alert(i18nUtils.prop("book_is_last_chapter"), 2);
         }
     },
-    initPlayBar:function(textId){
+    initPlayBar: function (textId) {
         var that = this;
-        if(textId === "bookDetailWrapper"){
+        if (textId === "bookDetailWrapper") {
             that.isMobile = true;
         }
-        that.$content = $("#"+textId);
+        that.$content = $("#" + textId);
 
         // if (typeof audios_path != "undefined" && audios_path != "") {
         //     var paths = audios_path.split(",");
@@ -207,7 +207,7 @@ AudioUtils = {
             $("input[name='audioPer'][value='" + st.per + "']").prop('checked', true);
         }
 
-        that.mark.$mark = new Mark("#"+textId);
+        that.mark.$mark = new Mark("#" + textId);
         that.playSpeechElement = $("#play_audio");
         that.playSpeechElement.click(function () {
             var status = that.playBarStatus();
@@ -223,16 +223,18 @@ AudioUtils = {
 
         try {
             var audioOpt = $("#audioOpt");
-            if(!that.isMobile){
+            if (!that.isMobile) {
                 var isAutoPlay = $.storage("auto-play");
-                if(isAutoPlay === "true"){
+                if (isAutoPlay === "true") {
                     $("#play_audio").click();
                 }
                 audioOpt.click(function () {
-                    layer.open({title: false, type: 1, anim: 2, shadeClose: true,
-                        area: ['300px', '179px'], content: $("#audioOptSet")});
+                    layer.open({
+                        title: false, type: 1, anim: 2, shadeClose: true,
+                        area: ['300px', '179px'], content: $("#audioOptSet")
+                    });
                 });
-            }else{
+            } else {
                 toggleOpt(audioOpt);
             }
             var audioSpeedRange = $("#audioSpeedRange")
@@ -241,7 +243,7 @@ AudioUtils = {
             });
             $("#audioSetLayout").on("click", '[type="radio"][name="audioPer"]', function () {
                 var t = $(this);
-                console.log("per:",t.val());
+                console.log("per:", t.val());
                 that.params.per = t.val();
                 that.updateAudioSettings({
                     per: t.val()
@@ -249,16 +251,16 @@ AudioUtils = {
             });
             audioSpeedRange.on("change", function (e) {
                 var t = this.value;
-                console.log("speed:",t);
+                console.log("speed:", t);
                 that.params.spd = t;
                 that.updateAudioSettings({
                     spd: t
                 })
             });
-        } catch(err) {
-            console.log("Auto play error：" +err)
-            var txt ="Auto play error：" + err.message+"\n\n";
-            that.alert(txt,2);
+        } catch (err) {
+            console.log("Auto play error：" + err)
+            var txt = "Auto play error：" + err.message + "\n\n";
+            that.alert(txt, 2);
         }
 
     },
@@ -268,30 +270,32 @@ AudioUtils = {
         v.audioSettings = $.extend(v.audioSettings, a)
         BookUtils.setStorage(v.audioSettingsKey, v.audioSettings);
     },
-    playAudio:function(i){
+    playAudio: function (i) {
         this.seek(i), this.loadAll();
         this.play();
     },
-    restartAudio:function(){
+    restartAudio: function () {
         this.audioList[0].pause();
         this.loadAll();
         this.play();
     },
-    alert:function(txt,type){
+    alert: function (txt, type) {
         var that = this;
-        if(!type){type=1}
-        if(layer){
+        if (!type) {
+            type = 1
+        }
+        if (layer) {
             if (that.isMobile) {
-                if(type === 1){
-                    layer.open({content: txt,skin: 'msg',time: 2});
+                if (type === 1) {
+                    layer.open({content: txt, skin: 'msg', time: 2});
                 } else {
-                    layer.open({content: txt,btn: 'Ok'});
+                    layer.open({content: txt, btn: 'Ok'});
                 }
             } else {
                 if (type === 1) {
                     layer.msg(txt);
                 } else {
-                    layer.open({content:txt, btn:"Ok"});
+                    layer.open({content: txt, btn: "Ok"});
                 }
             }
         } else {
@@ -303,24 +307,28 @@ AudioUtils = {
         try {
             for (var p = 0; p < that.params.preloadSize; p++) {
                 var v = new Audio();
-                v.addEventListener("play", function () {}),
-                    v.addEventListener("pause", function () {}),
-                    v.addEventListener("ended", function () {that.onAudioEnded()});
+                v.addEventListener("play", function () {
+                }),
+                    v.addEventListener("pause", function () {
+                    }),
+                    v.addEventListener("ended", function () {
+                        that.onAudioEnded()
+                    });
                 that.audioList.push(v)
             }
-        } catch(err) {
-            var txt =i18nUtils.prop("book_audio_play_not_support") + err.message+"\n\n";
-            txt+=i18nUtils.prop("book_audio_play_not_support_tip");
-            that.alert(txt,2);
+        } catch (err) {
+            var txt = i18nUtils.prop("book_audio_play_not_support") + err.message + "\n\n";
+            txt += i18nUtils.prop("book_audio_play_not_support_tip");
+            that.alert(txt, 2);
         }
 
     },
-    playBarStatus:function(){
+    playBarStatus: function () {
         var status = this.playSpeechElement.data("status");
         return status;
     },
-    initAudio:function(text){
-        if(!text || text.length <= 0){
+    initAudio: function (text) {
+        if (!text || text.length <= 0) {
             this.alert(i18nUtils.prop("book_audio_play_no_data"))
             return false;
         }
@@ -330,10 +338,10 @@ AudioUtils = {
         this.playAudio(this.curIndex);
         this.isFirstPlay = false;
     },
-    text2Audio: function(text){
-        console.log("text:{}",text);
+    text2Audio: function (text) {
+        console.log("text:{}", text);
         var that = this;
-        if(text && text.length <= 0){
+        if (text && text.length <= 0) {
             that.alert(i18nUtils.prop("book_audio_play_no_data"))
             return false;
         }
@@ -344,8 +352,8 @@ AudioUtils = {
         audio.src = ttsUrl + encodeURIComponent(stringList[0]);
         audio.play();
 
-        audio.onended = function() {
-            if(index < stringList.length){
+        audio.onended = function () {
+            if (index < stringList.length) {
                 audio.src = ttsUrl + encodeURIComponent(stringList[index]);
                 audio.play();
                 index++;
@@ -353,12 +361,13 @@ AudioUtils = {
         };
         return audio;
     },
-    extraText:function(n){var that = this;
+    extraText: function (n) {
+        var that = this;
         if (n.length <= that.params.maxChunkSize) return n;
         for (var e = []; n.length > that.params.maxChunkSize;) e.push(n.slice(0, that.params.maxChunkSize)), n = n.slice(that.params.maxChunkSize);
         return e.push(n), e
     },
-    initUrlList:function(text){
+    initUrlList: function (text) {
         var that = this;
         var n = text.replace(/<\/?([^>]*)>/g, "$1"), n = n.trim().replace(/\s+/g, " ");
         var u = n.split(/[。；？！：:.;?!]/);
@@ -392,11 +401,11 @@ AudioUtils = {
             timeout: 10000,
             hidden: true
         };
-        var opt = $.extend( optDefault, options );
+        var opt = $.extend(optDefault, options);
         // 调用语音合成接口
         // 参数含义请参考 https://ai.baidu.com/docs#/TTS-API/41ac79a6
         var paramDefault = {spd: 5, pit: 5, vol: 15, per: 0};
-        var p = $.extend( paramDefault, param );
+        var p = $.extend(paramDefault, param);
 
         // 如果浏览器支持，可以设置autoplay，但是不能兼容所有浏览器
         var audio = document.createElement('audio');
@@ -440,7 +449,7 @@ AudioUtils = {
 
         // 序列化参数列表
         var fd = [];
-        for(var k in data) {
+        for (var k in data) {
             fd.push(k + '=' + encodeURIComponent(data[k]));
         }
 
@@ -450,12 +459,12 @@ AudioUtils = {
         xhr.send(fd.join('&'));
 
         // 用timeout可以更兼容的处理兼容超时
-        var timer = setTimeout(function(){
+        var timer = setTimeout(function () {
             xhr.abort();
             isFunction(opt.onTimeout) && opt.onTimeout();
         }, timeout);
 
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 clearTimeout(timer);
                 if (xhr.status == 200) {
@@ -468,7 +477,7 @@ AudioUtils = {
 
                         // autoDestory设置则播放完后移除audio的dom对象
 
-                        audio.onended = function() {
+                        audio.onended = function () {
                             isFunction(opt.onEnded) && opt.onEnded();
                             if (opt.autoDestory) {
                                 document.body.removeChild(audio);
@@ -480,7 +489,7 @@ AudioUtils = {
 
                     // 用来处理错误
                     if (xhr.response.type === 'application/json') {
-                        frd.onload = function(){
+                        frd.onload = function () {
                             var text = frd.result;
                             isFunction(opt.onError) && opt.onError(text);
                         };
@@ -507,7 +516,8 @@ function rangeOptForAudio(t, a) {
             container: $("body"),
             shadow: !1,
             buttons: ["", ""],
-            onChangeEnd: function () {},
+            onChangeEnd: function () {
+            },
             tips: function (t) {
                 return t
             }
@@ -606,6 +616,7 @@ function rangeOptForAudio(t, a) {
         rangeOptValueForAudio(),
         this
 }
+
 function rangeOptValueForAudio(t) {
     var a = this.el_audio.input,
         n = a.val(),
@@ -620,6 +631,7 @@ function rangeOptValueForAudio(t) {
     t != n && a.trigger("change"),
         this
 }
+
 function rangeOptPositionForAudio() {
     var t = this.el_audio.input.val(),
         a = this.num_audio.max,
