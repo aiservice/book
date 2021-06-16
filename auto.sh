c73ed4e7a6a61ce9fff589fd36336b90ec899e9a
yw3 <yw3@abc.com>
2021-06-16 08:53:11 +0800

    commit_2021061608

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-commit_name="`date +%Y%m%d%H%k%M`_commit"
-echo "====git auto push start..."
+commit_name="commit_`date +%Y%m%d%H`"
+echo "====git auto push start...$commit_name"
 git add .
-git commit -m '$commit_name'
+git commit -m $commit_name
 git push
 echo "====git auto push end..."
 
