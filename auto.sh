4fd55730fac61c2c7cc6cdec5fa25aa3f199d020
yw3 <yw3@abc.com>
2021-06-16 08:46:19 +0800

    fix code

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-commit_name="`date +%Y%m%d%H%k%M`_commit"
+#commit_name="`date +%Y%m%d%H%k%M`_commit"
 echo "====git auto push start..."
 git add .
-git commit -m $commit_name
-git push origin master
+git commit -m 'fix code'
+git push
 echo "====git auto push end..."
 
--- scripts/index/site/19291.js
+++ scripts/index/site/19291.js
@@ -14,3 +14,5 @@ e_data_ad_300="3945536";
 site2898Urls = ["feiku"];
 ad_250_2898_img = "https://www.2898.com/upload/avatar/a8/a84b66eb6312f94b8022dd8457e3a83d.jpg";
 ad_250_2898_url = "http://p123.gysy2.com";
+
+ad_w2898_300_id = "w2898_14899";
