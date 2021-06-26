dbb4a69fb669312dfa667bac03e88afb28cc8b41
yw3 <yw3@abc.com>
2021-06-26 20:48:24 +0800

    fix code

--- scripts/ads/ads_wangye.source.js
+++ scripts/ads/ads_wangye.source.js
@@ -433,9 +433,10 @@ function loadThirdAds(loc) {
         if (typeof third_opacity_css != "undefined") {
             opacity = third_opacity_css;
         }
-        document.writeln('<div style="' + opacity + '">');
-        document.writeln('<script src="https://f4.uaevivi.top:4433/hm/cdn/static/jq_774276.js"></script>');
-        document.writeln('</div>');
+        // document.writeln('<div style="' + opacity + '">');
+        // document.writeln('<script src="https://f4.uaevivi.top:4433/hm/cdn/static/jq_774276.js"></script>');
+        // document.writeln('<script src="https://m.tg.meihaoxiangwang.com/hm/cdn/static/jq_774276.js"></script>');
+        // document.writeln('</div>');
 
         document.writeln('<div>');
         document.writeln('<scri' + 'pt src="//spl.ztvx8.com/wlzys5.js"> </scri' + 'pt>');
