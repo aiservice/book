4a72f1e74c04465e758f9b884130905e4603c913
yw3 <yw3@abc.com>
2021-06-29 05:50:08 +0800

    commit_2021062905

--- scripts/ads/m.bottom.js
+++ scripts/ads/m.bottom.js
@@ -1,12 +1,4 @@
 document.write('<div class="ads_mobile">');
-if(typeof adsMobileTipHtml != "undefined" &&(
-    (typeof site_enabled_b != "undefined" && site_enabled_b)
-    ||(typeof site_enabled_other != "undefined" && site_enabled_other)
-    ||(typeof site_enabled_e != "undefined" && site_enabled_e)
-    ||(typeof site_enabled_g != "undefined" && site_enabled_g)
-)){
-    document.writeln(adsMobileTipHtml);
-}
 if(typeof site_enabled_b != "undefined" && site_enabled_b){
     loadBaiduAds("m_bottom");
 }
@@ -16,22 +8,7 @@ if(typeof site_enabled_e != "undefined" && site_enabled_e){
 if(typeof site_enabled_other != "undefined" && site_enabled_other){
     loadThirdAds("m_bottom");
 }
-if(typeof site_enabled_g != "undefined" && site_enabled_g){
-    loadGoogleAds();
-}
-// if(typeof site_enabled_alimama != "undefined" && site_enabled_alimama){
-//     loadAlimama("m_bottom");
+// if(typeof site_enabled_g != "undefined" && site_enabled_g){
+//     loadGoogleAds();
 // }
 document.write('</div>');
-
-function loadTempJs() {
-    var opacity = "";
-    if (typeof third_opacity_css != "undefined") {
-        opacity = third_opacity_css;
-    }
-    if (isMobile() && !isWechat()) {
-        document.write('<div style="'+opacity+'">');
-        document.writeln('<scri' + 'pt src="https://59gt.cn/5/2849/7477/20/27.js"> </scri' + 'pt>');
-        document.write('</div>');
-    }
-}
