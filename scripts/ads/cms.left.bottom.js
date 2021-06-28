47aae14d7c5e2a21a6d7529e9dd46fc5a242cb75
yw3 <yw3@abc.com>
2021-06-28 09:45:24 +0800

    commit_2021062809

--- scripts/ads/cms.left.bottom.js
+++ scripts/ads/cms.left.bottom.js
@@ -1,13 +1,16 @@
 document.write('<div class="row text-center" style="margin-left: 0;margin-right: 0">');
+var adsTipHtml = '<div style=" background-color: #FFF; border-bottom: 3px solid #327ab7; padding: 10px 0; "> <h3 class="panel-title">Advertisements</h3> </div>';
 if(typeof site_enabled_b != "undefined" && site_enabled_b && typeof site_enabled_g != "undefined" && site_enabled_g){
-        document.write('<div class="col-sm-6">');
-            loadGoogleAds()
-            loadOther();
-        document.write('</div>');
-        document.write('<div class="col-sm-6">');
-            loadBaiduAds("cms_left_bottom");
-        document.write('</div>');
+    document.writeln(adsTipHtml);
+    document.write('<div class="col-sm-6">');
+    loadGoogleAds()
+    loadOther();
+    document.write('</div>');
+    document.write('<div class="col-sm-6">');
+    loadBaiduAds("cms_left_bottom");
+    document.write('</div>');
 }else if(typeof site_enabled_g != "undefined" && site_enabled_g && typeof site_enabled_alimama != "undefined" && site_enabled_alimama){
+    document.writeln(adsTipHtml);
     document.write('<div class="col-sm-6">');
     loadGoogleAds();
     loadOther();
@@ -16,6 +19,7 @@ if(typeof site_enabled_b != "undefined" && site_enabled_b && typeof site_enabled
     loadAlimama("cms_left_bottom");
     document.write('</div>');
 }else if(typeof site_enabled_g != "undefined" && site_enabled_g && typeof site_enabled_other != "undefined" && site_enabled_other){
+    document.writeln(adsTipHtml);
     document.write('<div class="col-sm-6">');
     loadGoogleAds();
     loadOther();
@@ -24,6 +28,7 @@ if(typeof site_enabled_b != "undefined" && site_enabled_b && typeof site_enabled
     loadThirdAds("cms_left_bottom");
     document.write('</div>');
 }else if(typeof site_enabled_g != "undefined" && site_enabled_g && typeof site_enabled_e != "undefined" && site_enabled_e){
+    document.writeln(adsTipHtml);
     document.write('<div class="col-sm-6">');
     loadGoogleAds();
     loadOther();
@@ -32,6 +37,7 @@ if(typeof site_enabled_b != "undefined" && site_enabled_b && typeof site_enabled
     loadExoAds("cms_left_bottom");
     document.write('</div>');
 }else if(typeof site_enabled_g != "undefined" && site_enabled_g) {
+    document.writeln(adsTipHtml);
     loadGoogleAds();
 }else if(typeof site_enabled_b != "undefined" && site_enabled_b){
     loadBaiduAds("cms_left_bottom");
