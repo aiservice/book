1ddb8d68c0b0ef10edb0f5b7dfe7f47108c9233f
yw3 <yw3@abc.com>
2021-07-08 08:41:56 +0800

    fix code

--- scripts/index/site/19290.js
+++ scripts/index/site/19290.js
@@ -1,9 +1,9 @@
-var app_domain = "www.aishen360.com";var is_english = false;var app_link_email = "admin@aishen360.com";var site_id = 19290;var baid = "f420afb31bbcd0748bc1974e69c28115";var gaid = "UA-40716497-6";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_468 = "2008793013";
+var app_domain = "www.aishen360.com";var is_english = false;var app_link_email = "admin@aishen360.com";var site_id = 19290;var baid = "f420afb31bbcd0748bc1974e69c28115";var gaid = "UA-40716497-6";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
+g_data_ad_slot_468 = "2008793013";
 g_data_ad_slot_recommend = "7657556062";
 g_data_ad_client = "ca-pub-4603891518763240";
 
-filterUrls = ["the-son-name.html","anzhaoshengjingxiaojingfumu.html","anzhaoshengjingxiaojingfumu.html","buyiyangderensheng.html","churentoudi.html","huodeyouya.html","pingdengzhongyouchayi-zh.html","cate-5008","category-5008","erzidemingfen-zh","biblical-theology-dictionary.html","biblical-theology-dictionary1.html","pingdengzhongyouchayi.html"];
-siteGUrls = ["aishen3"];
-// siteAlimamaUrls = ["aishen3"];
-siteOtherUrls = ["aishen3"];
+filterUrls = ["the-son-name.html","anzhaoshengjingxiaojingfumu.html","anzhaoshengjingxiaojingfumu.html","churentoudi.html","huodeyouya.html","erzidemingfen-zh","biblical-theology-dictionary.html","biblical-theology-dictionary1.html","yujidutongzuotianshang.html","children-ministry.html"];
+siteGUrls = ["aishen","feiku","sobq"];
+siteOtherUrls = ["aishen","feiku","sobq"];
 third_opacity_css  ="opacity: 0.03;";
--- scripts/index/site/56178.js
+++ scripts/index/site/56178.js
@@ -1,22 +1,9 @@
-var app_domain = "www.sobqg.com";var is_english = false;var app_link_email = "1740933979@qq.com";var site_id = 56178;var baid = "bd55e464f5f614540d14fa9f22b38b98";var gaid = "UA-40716497-21";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
+var app_domain = "www.sobqg.com";var is_english = false;var app_link_email = "1740933979@qq.com";var site_id = 56178;var baid = "bd55e464f5f614540d14fa9f22b38b98";var gaid = "UA-40716497-3";var ctx_cdn = "//cdn.book.moujishu.com";var cdn_api = "//cdn.book.moujishu.com";g_data_ad_slot_auto = "2008793013";
 g_data_ad_slot_468 = "2008793013";
 g_data_ad_slot_recommend = "7657556062";
 g_data_ad_client = "ca-pub-4603891518763240";
 
-filterUrls = ["cate-5008","category-5008","wanben","mingchaobaijiazi-zh","104691.html"];
-siteGUrls = ["aishen3","shitou","biquge"];
+filterUrls = ["cate-5008","category-5008"];
 
-o_data_ad_300="3549234227527096833-379";
-o_data_ad_300_right="3549234227527096833-385";
-o_data_ad_mobile="3549234227527096833-380";
-o_data_ad_728="3549234227527096833-381";
-siteOtherUrls = [];
-
-e_data_ad_300="3707189";
-e_data_ad_468="3707191";
-e_data_ad_160_600="3707193";
-e_data_ad_728="3801747";
-e_data_ad_mobile="3707199";
-siteEUrls = ["aishen3","shitou","biquge"];
-
-third_opacity_css  ="opacity: 0.3;"
+siteGUrls = ["aishen","feiku","sobq"];
+siteOtherUrls = ["aishen","feiku","sobq"];
