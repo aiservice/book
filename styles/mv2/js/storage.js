b3b60992c51cd6bfbb2a0c44dba8b2e22fc4bad5
yw3 <yw3@abc.com>
2021-07-10 23:09:46 +0800

    commit_2021071023

--- styles/mv2/js/storage.js
+++ styles/mv2/js/storage.js
@@ -1,67 +1,67 @@
-$.storage = function(key,value,expires) {
-    var Storage = function(){
+$.storage = function (key, value, expires) {
+    var Storage = function () {
         try {
-            localStorage.setItem('storage','');
+            localStorage.setItem('storage', '');
             localStorage.removeItem('storage');
             return localStorage;
-        } catch(e) { // 浏览器隐私模式下，例如iphone的Safari默认情况下，localstorage无法使用，所以使用cookie来代替
+        } catch (e) { // 浏览器隐私模式下，例如iphone的Safari默认情况下，localstorage无法使用，所以使用cookie来代替
             var cookies = document.cookie.split(";");
             return {
-                length : function(){
+                length: function () {
                     return cookies.length;
                 }(),
-                key : function(i){
+                key: function (i) {
                     var parts = cookies[i].split('=');
                     var name = decodeURIComponent(parts.shift());
                     return name;
                 },
-                getItem : function(key) {
+                getItem: function (key) {
                     return $.cookie(key);
                 },
-                setItem : function(key,value) {
-                    $.cookie(key,value,{expires:expires || 365*24*3600});
+                setItem: function (key, value) {
+                    $.cookie(key, value, {expires: expires || 365 * 24 * 3600});
                 },
-                removeItem : function(key) {
-                    $.cookie(key,null);
+                removeItem: function (key) {
+                    $.cookie(key, null);
                 },
-                clear : function() {
+                clear: function () {
                     $.cookie(null);
                 }
             };
         }
     }();
 
-    var data = {},time = parseInt((new Date()).getTime() / 1000);
+    var data = {}, time = parseInt((new Date()).getTime() / 1000);
     // 获取全部数据
-    if(key === undefined) {
+    if (key === undefined) {
         var k;
-        for(var i = 0;i < Storage.length;i ++) {
+        for (var i = 0; i < Storage.length; i++) {
             k = Storage.key(i);
             value = $.kv(k);
-            if(!value) continue;
+            if (!value) continue;
             data[k] = value;
         }
         return data;
     }
     // clear
-    else if(key === null) {
+    else if (key === null) {
         Storage.clear();
     }
     // read
-    else if(value === undefined) {
+    else if (value === undefined) {
         data = Storage.getItem(key);
-        if(!data) return false;
+        if (!data) return false;
 
         data = JSON.parse(data);
-        if(data.expires && time > data.expires) { // 如果过期
-            $.kv(key,null);
+        if (data.expires && time > data.expires) { // 如果过期
+            $.kv(key, null);
             return false;
         }
         data = data.value;
         return data;
     }
     // delete
-    else if(value === null) {
+    else if (value === null) {
         Storage.removeItem(key);
     }
     // set:add or update
@@ -73,7 +73,7 @@ $.storage = function(key,value,expires) {
         };
         data = JSON.stringify(data);
 
-        Storage.setItem(key,data);
+        Storage.setItem(key, data);
     }
 }
 
@@ -83,3 +83,42 @@ $.storage = function(key,value,expires) {
 //$.storage('name',null); // 删除键名name的值
 //$.storage('name','value'); // 添加或设置值
 //$.storage('name','value',3600); // 在设置值的时候，给一个生命周期值
+
+jQuery.cookie = function (name, value, options) {
+    if (typeof value != 'undefined') { // name and value given, set cookie
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+        }
+        var path = options.path ? '; path=' + options.path : '';
+        var domain = options.domain ? '; domain=' + options.domain : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else { // only name given, get cookie
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
--- styles/v3/js/chapter.source.js
+++ styles/v3/js/chapter.source.js
@@ -1,82 +1,176 @@
-function cookie_encode(a) {
-    var b = encodeURIComponent(a),
-        c = b.replace(/(%7B|%7D|%3A|%22|%23|%5B|%5D)/g,
-            function (a) {
-                return decodeURIComponent(a)
-            });
-    return c
-}
-
 function thmoon() {
-    document.getElementById("readbg").className = "gd theme-moon active",
-        $.cookie("thmoon", "1", {
-            expires: 365,
-            path: "/"
-        })
+    document.getElementById("readbg").className = "gd theme-moon active";
+    BookUtils.setStorage("thmoon", "1");
 }
 
 function clsize(a) {
-    15 == a ? document.getElementById("TextContent").className = "read-content fz14" : 16 == a ? document.getElementById("TextContent").className = "read-content fz16" : 17 == a ? document.getElementById("TextContent").className = "read-content fz18" : 18 == a ? document.getElementById("TextContent").className = "read-content fz20" : 19 == a ? document.getElementById("TextContent").className = "read-content fz22" : 20 == a && (document.getElementById("TextContent").className = "read-content fz24"),
-        $.cookie("fontsize", a, {
-            expires: 365,
-            path: "/"
-        })
+    15 == a ? document.getElementById("TextContent").className = "read-content fz14" : 16 == a ? document.getElementById("TextContent").className = "read-content fz16" : 17 == a ? document.getElementById("TextContent").className = "read-content fz18" : 18 == a ? document.getElementById("TextContent").className = "read-content fz20" : 19 == a ? document.getElementById("TextContent").className = "read-content fz22" : 20 == a && (document.getElementById("TextContent").className = "read-content fz24");
+    BookUtils.setStorage("fontsize", a);
 }
 
 function cltheme(a) {
     if (!a) {
         a = 7;
     }
-    1 == a ? document.getElementById("readbg").className = "hui" : 2 == a ? document.getElementById("readbg").className = "ls" : 3 == a ? document.getElementById("readbg").className = "fs" : 4 == a ? document.getElementById("readbg").className = "nuan" : 5 == a ? document.getElementById("readbg").className = "huang" : 6 == a ? document.getElementById("readbg").className = "lan" : 7 == a && (document.getElementById("readbg").className = "nuan"),
-        $.cookie("theme", a, {
-            expires: 365,
-            path: "/"
-        })
+    1 == a ? document.getElementById("readbg").className = "hui" : 2 == a ? document.getElementById("readbg").className = "ls" : 3 == a ? document.getElementById("readbg").className = "fs" : 4 == a ? document.getElementById("readbg").className = "nuan" : 5 == a ? document.getElementById("readbg").className = "huang" : 6 == a ? document.getElementById("readbg").className = "lan" : 7 == a && (document.getElementById("readbg").className = "nuan");
+    BookUtils.setStorage("theme", a);
+    BookUtils.removeStorage("thmoon");
 }
 
-function theme() {
-    document.writeln('<div class="toolbar"><div class="theme" style="float: left;width: auto;height: auto;"><span><a onclick="sq(' + bid + "," + cid + ');">添加书签</a><a href="/newmessage.php?tosys=1&amp;title=' + name + ' 有错误&content=https://www.yoduzw.com/modules/article/chapteredit.php?id=' + cid + '%0D%0A请明确错误原因%0D%0A%0D%0A">报错求书</a><a href="/history">阅读记录</a></span></div><a href="javascript:;" class="aminus font_dec" id="font_dec"></a><a href="javascript:;" class="aadd font_inc" id="font_inc"></a><a href="javascript:;" class="pattern menu-moon" id="mode"></a><div class="option theme"><div class="theme-area theme-pink" id="theme2"></div></div><div class="cr"></div></div>')
+$(function () {
+    var c = BookUtils.getStorage("thmoon"), a = BookUtils.getStorage("fontsize"),
+        b = BookUtils.getStorage("theme");
+    if (b) {
+        cltheme(b);
+    } else {
+        thmoon();
+    }
+    if (c) {
+        thmoon();
+    }
+    void 0 == a ? (clsize(18), a = 18) : clsize(a),
+        // void 0 == b ? (cltheme(1), b = 1) : cltheme(b),
+        $(".set_font_inc").click(function () {
+            19 >= a && "" != a ? (a++, clsize(a)) : clsize("20")
+        }),
+        $(".set_font_dec").click(function () {
+            a >= 16 && "" != a ? (a--, clsize(a)) : clsize("15")
+        }),
+        $(".set_theme").click(function () {
+            6 >= b ? (b++, cltheme(b)) : (b -= 6, cltheme(b))
+        }),
+        // 1 == c && thmoon(),
+        $("#mode").click(function () {
+            var a = document.getElementById("readbg").getAttribute("class");
+            if ("gd theme-moon active" !== a) {
+                thmoon()
+            } else {
+                cltheme(b);
+            }
+        });
+    BookChapterUtils.init();
+});
+
+if (typeof BookChapterUtils == "undefined") {
+    BookChapterUtils = {};
 }
-
-jQuery.cookie = function (a, b, c) {
-    var d, e, f, g;
-    return arguments.length > 1 && "[object Object]" !== String(b) ? (c = jQuery.extend({},
-        c), (null === b || void 0 === b) && (c.expires = -1), "number" == typeof c.expires && (d = c.expires, e = c.expires = new Date, e.setDate(e.getDate() + d)), b = String(b), document.cookie = [encodeURIComponent(a), "=", c.raw ? b : cookie_encode(b), c.expires ? "; expires=" + c.expires.toUTCString() : "", c.path ? "; path=" + c.path : "", c.domain ? "; domain=" + c.domain : "", c.secure ? "; secure" : ""].join("")) : (c = b || {},
-        g = c.raw ?
-            function (a) {
-                return a
-            } : decodeURIComponent, (f = new RegExp("(?:^|; )" + encodeURIComponent(a) + "=([^;]*)").exec(document.cookie)) ? g(f[1]) : null)
-},
-    $(function () {
-        var c = $.cookie("thmoon"), a = $.cookie("fontsize"),
-            b = $.cookie("theme");
-        if (b) {
-            cltheme(b);
-        } else {
-            thmoon();
+BookChapterUtils = {
+    params: {
+        settingsJson: {dn: "d", fs: 3, skin: 0},
+        catalog_cache: false,
+        settingsKey: "SET_CHAPTER"
+    },
+    init: function () {
+        this.initModes();
+        this.initDayNightSetting();
+        this.initFontSizeSetting();
+        this.initAudio();
+    },
+    initModes: function () {
+        var that = this, o = $("#readBtnMode");
+        var dn = BookUtils.getStorage("thmoon");
+        var skin = BookUtils.getStorage("theme");
+        //console.log("set1:", v.settingsJson)
+        //console.log("setting in cookie:", st);
+        if (dn) {
+            console.log("dn:", dn);
+            o.data("mode", "night")
+        } else if (skin) {
+            o.data("mode", "day");
+            console.log("skin:", skin);
+            var rd = $('[data-index="' + skin + '"]', "#readSetSkin"),
+                skin_value = rd.val();
+            rd.attr("checked", "true");
         }
-        if (c) {
-            thmoon();
-        }
-        void 0 == a ? (clsize(18), a = 18) : clsize(a),
-            // void 0 == b ? (cltheme(1), b = 1) : cltheme(b),
-            $("#font_inc").click(function () {
-                19 >= a && "" != a ? (a++, clsize(a)) : clsize("20")
-            }),
-            $("#font_dec").click(function () {
-                a >= 16 && "" != a ? (a--, clsize(a)) : clsize("15")
-            }),
-            $("#theme2").click(function () {
-                6 >= b ? (b++, cltheme(b)) : (b -= 6, cltheme(b))
-            }),
-            // 1 == c && thmoon(),
-            $("#mode").click(function () {
-                var a = document.getElementById("readbg").getAttribute("class");
-                "gd theme-moon active" != a ? thmoon() : (cltheme(b), $.cookie("thmoon", null, {
-                    expires: 365,
-                    path: "/"
-                }))
-            })
+
+        var isClick = true;
+        var pageReadOpt = $("#pageReadOpt");
+        $(".mlfy_main").on('click', function () {
+            console.log("isClick:", isClick);
+            if (isClick) {
+                setTimeout(function () {
+                    pageReadOpt.toggleClass("active");
+                    isClick = true;
+                }, 100);
+            }
+            isClick = false;
+        });
+    },
+    initFontSizeSetting: function () {
+        var that = this,
+            a = $("#readBtnSet");
+        toggleOpt(a);
+    },
+    initAudio: function () {
+        var audioOpt = $("#audioOptMobile");
+        audioOpt.on("click", function () {
+            console.log($("#audioOptSet").length)
+            $("#audioOptSet").addClass("active")
+        });
+    },
+    initDayNightSetting: function () {
+        var that = this,
+            o = $("#readBtnMode"),
+            r = o.find("h4");
+        o.on("click", function () {
+            var t = o.data("mode");
+            console.log(t);
+            if ("day" === t) {
+                cltheme(4);
+                r.html(r.html().replace("日", "夜")), r.html(r.html().replace("Day", "Night"));
+                o.find(".icon-day").hide();
+                o.find(".icon-night").show();
+                o.data("mode", "night");
+            } else {
+                thmoon();
+                r.html(r.html().replace("夜", "日")), r.html(r.html().replace("Night", "Day"));
+                o.data("mode", "day");
+                o.find(".icon-day").show();
+                o.find(".icon-night").hide();
+            }
+        });
+        o.trigger("click");
+    },
+
+};
 
 
+function toggleOpt(t, a) {
+    var i = this;
+    if (!t) return i;
+    $.isFunction(a) && (a = {
+        callback: a
     });
+    var o = {
+        mode: "visible",
+        container: $("body"),
+        callback: function () {
+        }
+    }, n = $.extend({}, o, a || {});
+    i.callback = n.callback, i.mode = n.mode, !t.size && $.isArray(t) && (t = t.join()),
+        "string" == typeof t ? n.container.on("click", t, function (t) {
+            toggleOptToggle($(this), t);
+        }) : t.length && t.on("click", function (t) {
+            toggleOptToggle($(this), t);
+        }), toggleOptAria(t);
+}
+
+function toggleOptAria(t) {
+    "visible" == this.mode && "string" == typeof t && $(t).each(function () {
+        var t = $(this);
+        t.attr({
+            role: "menuitem",
+            "aria-expanded": t.hasClass("active")
+        });
+    });
+}
+
+function toggleOptToggle(t, a) {
+    var i, o = this,
+        n = t;
+    "visible" == o.mode ? (n = $("#" + t.attr("data-rel")), (i = t.hasClass("active")) ? (t.removeClass("active").attr(
+        "aria-expanded", "false"),
+        n.removeClass("active")) : (t.addClass("active").attr("aria-expanded", "true"), n.addClass("active"))) : "more" ==
+        o.mode && ((i = "string" == typeof t.attr("open")) ? t.removeAttr("open") : t.attr("open", ""))
+    //,o.callback.call(t, t, n, i, a);
+}
